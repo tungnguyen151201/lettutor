063d465cb72962ae2057dcd6ae9fac5d500fbc7f
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2023-01-08 13:51:04 +0700

    fix https certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:lettutor/providers/app_provider.dart';
 import 'package:provider/provider.dart';
@@ -5,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'screens/authentication/login.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -18,5 +21,14 @@ class MyApp extends StatelessWidget {
         create: (_) => AppProvider(),
       ),
     ], child: const MaterialApp(home: LoginScreen()));
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
